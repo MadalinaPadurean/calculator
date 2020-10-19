7fff13a433f63400b64f936706766f5e02441708
Madalina Padurean <madapadurean@yahoo.com>
2020-10-19 18:48:23 +0300

    overloaded calculator methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -3,20 +3,21 @@ package org.fasttrackit;
 public class App
 {
     public static void main( String[] args ) {
+
         Calculator calculator = new Calculator();
-        int x = 135;
-        int y = 632;
+        double m = 135.65;
+        double n = 632.34;
 
-        System.out.println("Calculating the sum of " +  x + " and" + y);
-        int sum = calculator.sum(x,y);
-        System.out.println("The sum is " + sum);
+        System.out.println("Calculating the sum of " +  m + " and" + n);
+        double doubleSum = calculator.sum(m,n);
+        System.out.println("The sum is " + doubleSum);
 
-        System.out.println("Calculating the product of " + x + " and" + y);
-        int product = calculator.multiply(x,y);
-        System.out.println("The product is" + product);
+        System.out.println("Calculating the product of " + m + " and" + n);
+        double doubleProduct = calculator.multiply(m, n);
+        System.out.println("The product is" + doubleProduct);
 
-        System.out.println("Dividing " + x + " by " + y);
-        double result = calculator.divide(x,y);
-        System.out.println("The result is " + result);
+        System.out.println("Dividing " + m + " by " + n);
+        double doubleResult = calculator.divide(m,n);
+        System.out.println("The result is " + doubleResult);
         }
     }
--- src/main/java/org/fasttrackit/Calculator.java
+++ src/main/java/org/fasttrackit/Calculator.java
@@ -1,13 +1,14 @@
 package org.fasttrackit;
 
 public class Calculator {
-    public int sum(int a, int b) {
+    public double sum(double a, double b) {
         return a + b;
     }
-    public int multiply(int a, int b) {
+    public double multiply(double a, double  b) {
         return a * b;
     }
     public double divide(double a, double b) {
+
         return a / b;
     }
 }
